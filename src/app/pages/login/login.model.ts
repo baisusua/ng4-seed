ff8a44d30022df61188e0fc154ebb08dd66f9cec
kround <yuan40kang@126.com>
2018-01-16 16:50:27 +0800

    删除无用文件

--- src/app/pages/login/login.model.ts
+++
@@ -1,8 +0,0 @@
-export class LoginModel {
-    constructor(
-        public username: string = '',
-        public pwd: string = '',
-        public captcha ?: string,
-        public success: boolean = true
-    ) {}
-}
