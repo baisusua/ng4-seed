9189e61e03613ec897bff188136091b2757f0e6c
kround <yuan40kang@126.com>
2018-01-16 18:57:32 +0800

    支持上传cdn以及gitlab自动部署功能

--- task/gulp-build/index.js
+++ task/gulp-build/index.js
@@ -3,7 +3,7 @@ const gutil = require("gulp-util");
 const webpack = require('webpack');
 const config = require('../config.json');
 const type = config[process.argv[2]] ? process.argv[2] : 'dev';
-const WebpackConfig = require('../webpack.prod')('dev');
+const WebpackConfig = require('../webpack.prod')(type);
 
 gulp.task('webpack', function (callback) {
     webpack(WebpackConfig, function (err, stats) {
