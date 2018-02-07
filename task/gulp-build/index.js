117e3074498621d53c6502036ec8045be4cfe17d
kround <yuan40kang@126.com>
2018-02-07 16:31:34 +0800

    忽略config 配置

--- task/gulp-build/index.js
+++ task/gulp-build/index.js
@@ -4,7 +4,6 @@ const webpack = require('webpack');
 const config = require('../config.json');
 const type = config[process.argv[2]] ? process.argv[2] : 'dev';
 const WebpackConfig = require('../webpack.prod')(type);
-
 gulp.task('webpack', function (callback) {
     webpack(WebpackConfig, function (err, stats) {
         if (err) throw new gutil.PluginError("webpack", err);
